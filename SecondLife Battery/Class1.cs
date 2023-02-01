82f62e42326889c59550285d1ded7b1e84ec84df
FilipWarpman <Filip.warpman@gmail.com>
2023-02-01 14:47:46 +0100

    Add project files.

---
+++ SecondLife Battery/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondLife_Battery
+{
+    internal class Class1
+    {
+        static void main(string[] args)
+        {
+            System.Console.WriteLine("hello"); 
+        }
+
+    }
+}
