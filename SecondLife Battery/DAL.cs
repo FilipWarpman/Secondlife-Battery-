3c04256e6372982f8a908f59a4f861a8d9294ce1
simonolsson11 <simon.olsson11@hotmail.com>
2023-02-06 11:42:57 +0100

    Started with DAL

---
+++ SecondLife Battery/DAL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondLife_Battery
+{
+    public class DAL
+    {
+        private string connectionString = @"Server=localhost;Database=dbAssignment;user=SecondLife;password=andraliv;";
+
+        public void getWeather() {
+            
+            SqlConnection connection = new SqlConnection(connectionString);
+
+    }
+        
+
+    }
+}
